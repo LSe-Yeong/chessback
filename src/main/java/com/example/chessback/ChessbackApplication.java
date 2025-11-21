b7ad407b512565234ba850e76f3e8b43b709178b
Lse-Yeong <ocdee39@gmail.com>
2025-11-21 15:52:12 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/chessback/ChessbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.chessback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chessback/ChessbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chessback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

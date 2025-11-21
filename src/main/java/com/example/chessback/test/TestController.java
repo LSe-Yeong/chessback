4da7ab20ff1cf541a29fa22b2322af0f6f3e7d51
Lse-Yeong <ocdee39@gmail.com>
2025-11-21 16:55:44 +0900

    feat: STOMP를 활용한 실시간 통신 기능 구현

--- src/main/java/com/example/chessback/test/TestController.java
+++ src/main/java/com/example/chessback/test/TestController.java
@@ -1,5 +1,7 @@
 package com.example.chessback.test;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,12 @@ public class TestController {
     public String test() {
         System.out.println("request detected");
         return "Hello, World";
+    }
+
+    @MessageMapping("/chat/message")
+    @SendTo("/sub/chat/room")
+    public String stompTest() {
+        System.out.println("STOMP detected");
+        return "STOMP connect HELLO";
     }
 }

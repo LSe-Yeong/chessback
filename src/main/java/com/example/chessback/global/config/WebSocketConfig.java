0c67547a7f0873e904048b5965624b898a1745b6
Lse-Yeong <ocdee39@gmail.com>
2025-11-22 19:00:23 +0900

    feat: 최대 2명까지만 한 방에 입장 가능하도록 제한 기능 구현

--- src/main/java/com/example/chessback/global/config/WebSocketConfig.java
+++ src/main/java/com/example/chessback/global/config/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.example.chessback.global.config;
 
+import com.example.chessback.global.handler.ConnectionLimitHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -21,5 +23,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableSimpleBroker("/sub");
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+        registry.addDecoratorFactory(handler -> new ConnectionLimitHandler(handler));
     }
 }

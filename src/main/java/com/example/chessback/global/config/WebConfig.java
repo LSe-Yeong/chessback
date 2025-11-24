61e84ed04b4353d51d0402fb9953a3c3bdc3c881
Lse-Yeong <ocdee39@gmail.com>
2025-11-24 18:45:07 +0900

    chore: vercel 브라우저 cors 추가

--- src/main/java/com/example/chessback/global/config/WebConfig.java
+++ src/main/java/com/example/chessback/global/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://chessfront-drab.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true)
                         .allowedHeaders("*")
